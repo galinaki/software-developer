5c7d5f72a42eb78a061d3bcc46d33af97fada487
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-03-11 14:19:37 -0500

    links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 import Introduce from './components/Introduction/Introduce'
 import Projects from './components/projects/Projects';
 import Resume from './components/resume/Resume';
@@ -6,9 +8,18 @@ import Resume from './components/resume/Resume';
 function App() {
   return (
     <div className="App">
+      <Header />
+      <br />
       <Introduce />
+      <br />
+      <br />
       <Projects />
+      <br />
+      <br />
       <Resume />
+      <br />
+      <br />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../footer/Footer'
+import Projects from '../projects/Projects'
+import Resume from '../resume/Resume'
+
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li><a href='#projects'>My projects</a></li>
+        <li><a href='#resume'>Resume</a></li>
+        <li><a href='#footer'>Contact me</a></li>
+      </ul>
+    </div>
+  )
+}
