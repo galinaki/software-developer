f3b9cfaadf61f8c2f1b790ba6e1fe4ed797a7b6f
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-03-11 12:32:18 -0500

    projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Introduce from './components/Introduction/Introduce'
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Introduce />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div>
+      <h2>My projects</h2>
+      <h4>Lyrics mood</h4>
+      <a href='https://galinaki.github.io/Lyrics/'>Click here</a>
+      <h5><a href='https://github.com/galinaki/Lyrics'>github</a></h5>
+      <h4>Name's secrets</h4>
+      <a href='https://galinakish-names-secrets.netlify.app/'>Click here</a>
+      <h5><a href='https://github.com/galinaki/Secrets'>github</a></h5>
+      <h4>L.A.G.A - Life After General Assembly</h4>
+      <a href='https://silly-kowalevski-1591fc.netlify.app/'>Click here</a>
+      <h5><a href='https://github.com/galinaki/proyecto-tres'>back-end github</a></h5>
+      <h5><a href='https://github.com/bannersamuelson/Life-After-General-Assembly'>front-end github</a></h5>
+      <h4>Save Animals</h4>
+      <a href='https://galinak-saveanimals.netlify.app/animals'>Click here</a>
+      <h5><a href='https://github.com/galinaki/savelives'>github</a></h5>
+    </div>
+  )
+}
