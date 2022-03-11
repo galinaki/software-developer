e44ddde4d0126a406f81c7af9e26577240444252
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-03-11 12:09:35 -0500

    restart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Introduce from './components/Introduction/Introduce'
+
+function App() {
+  return (
+    <div className="App">
+      <Introduce />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Introduction/Introduce.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Introduce() {
+
+  return (
+    <div>
+      <h2>Galina</h2>
+      <h2>Kishirova</h2>
+      <h3>Software developer</h3>
+    </div>
+  );
+}
