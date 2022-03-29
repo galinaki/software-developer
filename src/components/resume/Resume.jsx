d92c6ed809de0a638ba5a738f517cd10f131600a
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-03-28 22:31:12 -0400

    resume

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -5,7 +5,8 @@ export default function Resume() {
   return (
     <div id='resume' className={styles.resume}>
       <h2>Resume</h2>
-      <a href='https://drive.google.com/file/d/1srVpdDdXHnbFqz1M3oJPXo94LarJbY7z/view?usp=sharing'>see here</a>
+      <a href='https://drive.google.com/file/d/1srVpdDdXHnbFqz1M3oJPXo94LarJbY7z/view?usp=sharing'><img src='../../img/GalinaKishirova.jpg' width='100%' alt='resume'/></a>
+    
     </div>
   )
 }
